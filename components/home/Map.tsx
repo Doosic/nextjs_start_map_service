3641440f8cbcfafa0d8b4ca437804cdd4cb20328
doosicEE <strou73@naver.com>
2023-03-07 14:08:49 +0900

    #Add NaverMap
    네이버 맵 메인페이지(index.tsx)에 적용

---
+++ components/home/Map.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from 'react';
+import Script from 'next/script';
+import { Coordinates } from '../../types/store';
+import { NaverMap } from '../../types/map';
+import { INITIAL_CENTER, INITIAL_ZOOM } from '../../hooks/useMap';
+
+type Props = {
+  mapId?: string;
+  initialCenter?: Coordinates;
+  initialZoom?: number;
+  onLoad?: (map: NaverMap) => void;
+};
+
+const Map = ({
+  mapId = 'map',
+  initialCenter = INITIAL_CENTER,
+  initialZoom = INITIAL_ZOOM,
+  onLoad,
+}: Props) => {
+  const mapRef = useRef<NaverMap | null>(null);
+
+  const initializeMap = () => {
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(...initialCenter),
+      zoom: initialZoom,
+      minZoom: 9,
+      scaleControl: false,
+      mapDataControl: false,
+      logoControlOptions: {
+        position: naver.maps.Position.BOTTOM_LEFT,
+      },
+    };
+
+    const map = new window.naver.maps.Map(mapId, mapOptions);
+    mapRef.current = map;
+
+    if (onLoad) {
+      onLoad(map);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      mapRef.current?.destroy();
+    };
+  }, []);
+  // onLoad 는 Script가 load되었을때만 실행된다. 그러므로 script가 존재하는 component가 mount 될때마다 실행되는 onReady를 사용한다.
+  // strategy="afterInteractive" : 페이지가 로드되자마자 보여야 한다면 해당 옵션을 사용하는것이 좋다.
+  // 더 빨라야 한다면 beforeInteractive를 사용한다.(다만 모든 페이지에 사용하는 아주 중요한것만 사용하기를 권장)
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}
+        onReady={initializeMap}
+      />
+      <div id={mapId} style={{ width: '100%', height: '100%' }} />
+    </>
+  );
+};
+
+export default Map;
