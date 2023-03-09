f12f2e0d9c39323b019609806db929ac80cacbc4
DoosicEE <strou73@naver.com>
2023-03-10 04:12:59 +0900

    #Update 지도 및 마커 구현완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import { Fragment, useEffect } from 'react';
-import Header from '../components/common/Header';
 import MapSection from '../components/home/MapSection';
-import styles from '../styles/header.module.scss';
-import Link from 'next/link';
-import { AiOutlineShareAlt } from 'react-icons/ai';
-import { VscFeedback } from 'react-icons/vsc';
+import HomeHeader from '../components/home/Header';
 import { Store } from '../types/store';
 import useStores from '../hooks/useStores';
 import { NextPage } from 'next';
@@ -22,22 +18,7 @@ const Home: NextPage<Props> = ({ stores }) => {
 
   return (
     <Fragment>
-      <Header
-        rightElements={[
-          <button
-            onClick={() => {
-              alert('복사!');
-            }}
-            className={styles.box}
-            key="button"
-          >
-            <AiOutlineShareAlt size={20} />
-          </button>,
-          <Link href="/feedback" className={styles.box} key="link">
-            <VscFeedback size={20} />
-          </Link>,
-        ]}
-      />
+      <HomeHeader />
       <main style={{ width: '100%', height: '100%' }}>
         <MapSection />
       </main>
